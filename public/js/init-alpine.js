68e8feecac9d535ee8ee33269fa38c0beae33e90
RamyNagy2025 <83719788+RamyNagy2025@users.noreply.github.com>
2022-01-31 15:23:47 +0200

    make auth

--- public/js/init-alpine.js
+++ public/js/init-alpine.js
@@ -1,26 +1,66 @@
-document.addEventListener('alpine:init', () => {
-    Alpine.data('data', () => ({
-        isProfileMenuOpen: false,
-        toggleProfileMenu() {
-            this.isProfileMenuOpen = !this.isProfileMenuOpen
-        },
+function data() {
+    function getThemeFromLocalStorage() {
+        // if user already changed the theme, use it
+        if (window.localStorage.getItem("dark")) {
+            return JSON.parse(window.localStorage.getItem("dark"));
+        }
 
-        closeProfileMenu() {
-            this.isProfileMenuOpen = false
-        },
+        // else return their preferences
+        return (
+            !!window.matchMedia &&
+            window.matchMedia("(prefers-color-scheme: dark)").matches
+        );
+    }
 
+    function setThemeToLocalStorage(value) {
+        window.localStorage.setItem("dark", value);
+    }
+
+    return {
+        dark: getThemeFromLocalStorage(),
+        toggleTheme() {
+            this.dark = !this.dark;
+            setThemeToLocalStorage(this.dark);
+        },
         isSideMenuOpen: false,
         toggleSideMenu() {
-            this.isSideMenuOpen = !this.isSideMenuOpen
+            this.isSideMenuOpen = !this.isSideMenuOpen;
         },
-
         closeSideMenu() {
-            this.isSideMenuOpen = false
+            this.isSideMenuOpen = false;
         },
-
+        isNotificationsMenuOpen: false,
+        toggleNotificationsMenu() {
+            this.isNotificationsMenuOpen = !this.isNotificationsMenuOpen;
+        },
+        closeNotificationsMenu() {
+            this.isNotificationsMenuOpen = false;
+        },
+        isProfileMenuOpen: false,
+        toggleProfileMenu() {
+            this.isProfileMenuOpen = !this.isProfileMenuOpen;
+        },
+        closeProfileMenu() {
+            this.isProfileMenuOpen = false;
+        },
+        isPagesMenuOpen: false,
+        togglePagesMenu() {
+            this.isPagesMenuOpen = !this.isPagesMenuOpen;
+        },
+        // Modal
+        isModalOpen: false,
+        trapCleanup: null,
+        openModal() {
+            this.isModalOpen = true;
+            this.trapCleanup = focusTrap(document.querySelector("#modal"));
+        },
+        closeModal() {
+            this.isModalOpen = false;
+            this.trapCleanup();
+        },
         isMultiLevelMenuOpen: false,
         toggleMultiLevelMenu() {
             this.isMultiLevelMenuOpen = !this.isMultiLevelMenuOpen
         }
-    }))
-})
+    };
+}
